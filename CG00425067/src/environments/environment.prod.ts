74b0b700dd6cfe9adb811e6bf004d2c3ee72d1bc
TiffanyYongNgikChee <g00425067@atu.ie>
2025-04-18 14:19:44 +0000

    Add GarageService to fetch vehicles from Spring Boot API

---
+++ CG00425067/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+
+export const environment = {
+    production: true,
+    garage_api_url: 'localhost:8080/api',
+  
+  };
---
+++ CG00425067/src/environments/environment.ts
@@ -0,0 +1,6 @@
+
+export const environment = {
+    production: false,
+    garage_api_url: 'localhost:8080/api',
+  
+  };
