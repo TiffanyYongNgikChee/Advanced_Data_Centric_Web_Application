9ef5beea46a53d1a01d0ccbe14b43240c44c7a44
TiffanyYongNgikChee <g00425067@atu.ie>
2025-04-18 14:27:44 +0000

    generate component for display vehicle on home page

--- CG00425067/src/app/app.routes.ts
+++ CG00425067/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { DetailsComponent } from './components/details/details.component';
+import { VehicleComponent } from './components/vehicle/vehicle.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'vehicles', component: VehicleComponent },
+    { path: 'vehicleDetails', component: DetailsComponent },
+    { path: '', redirectTo: '/vehicles', pathMatch: 'full' },
+    { path: '**', redirectTo: '/vehicles' }
+];
